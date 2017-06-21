a7c746ad12bf5b4b0cea11bd5cd5fade810a9fbf
panagiopa <panagiopa@ceid.upatras.gr>
2017-06-21 19:57:33 +0300

    Update to message

--- app/src/main/java/com/upatras/android/iot_secure/MainActivity.java
+++ app/src/main/java/com/upatras/android/iot_secure/MainActivity.java
@@ -17,7 +17,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
